b8133d39dcb2450c992e0352c470ae231ccffcc7
Arayik Gevorgyan <72850471+ArayikGevorgyan@users.noreply.github.com>
2024-10-02 13:09:29 +0400

    Update Algorithms-HW1.py
    Homework 2.

--- Algorithms-HW1.py
+++ Algorithms-HW1.py
@@ -58,8 +58,8 @@ stack.pop()
 
 print(stack)
 
-#-----------------------------------------------------------------------------------------------------------------------
-                                            #3)Additional Tasks
+# #-----------------------------------------------------------------------------------------------------------------------
+#                                             #3)Additional Tasks
 
 #   -Print all elements of the list.
 my_list = [1, 2, 3, 4, 5]
@@ -104,3 +104,62 @@ num_elements = len(array)
 average = total_sum / num_elements
 
 print(average)
+
+#-----------------------------------------------------------------------------------------------------------------------
+                                                      #Homework 2
+                                      #Implement Insertion Sort recursively.
+
+
+def insertion_sort_recursive(arr, n):
+    if n <= 1:
+        return
+
+    insertion_sort_recursive(arr, n-1)
+    last = arr[n-1]
+    j = n-2
+
+    while j >= 0 and arr[j] > last:
+        arr[j+1] = arr[j]
+        j -= 1
+
+    arr[j+1] = last
+
+arr = [12, 11, 13, 5, 6]
+insertion_sort_recursive(arr, len(arr))
+print("Sorted array is:", arr)
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+                                        #Implement a Custom Queue using a list.
+
+class CustomQueue:
+    def __init__(self):
+        self.queue = []
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Dequeue from an empty queue")
+        return self.queue.pop(0)
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Peek from an empty queue")
+        return self.queue[0]
+
+    def size(self):
+        return len(self.queue)
+
+queue = CustomQueue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+print("Dequeued item is:", queue.dequeue())
+print("Peek item is:", queue.peek())
+print("Queue size is:", queue.size())
